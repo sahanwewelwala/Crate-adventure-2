bad67199a2bb66b09ec06ad101c4d1416ee0b4a5
sahanwewelwala <sahanwewelwala@gmail.com>
2021-06-08 21:24:28 +0530

    Seconnd commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function changeText(idElement) {
+    var element = document.getElementById('element' + idElement);
+    if (idElement === 1 ) {
+        if (element.innerHTML === 'VIEW MORE') element.innerHTML = 'Close';
+        else {
+            element.innerHTML = 'VIEW MORE';
+        }
+    }
+}
