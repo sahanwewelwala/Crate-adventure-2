fbc8adfe9d17a532993fff2a2e470a503344bc37
sahanwewelwala <sahanwewelwala@gmail.com>
2021-06-18 10:05:33 +0530

    tenth   commit

--- js/script.js
+++ js/script.js
@@ -2,11 +2,20 @@ function clearcontent(elementID) {
     document.getElementById(elementID).innerHTML = "";
     const img = document.getElementById("image-gallery-1");
     img.src = "./assets/gallery-4.PNG";
+    var abc = '#';
+    document.getElementById('clear').innerHTML = '<a href="' + abc + '"></a>';
+    
+
 }
 function addcontent(elementID) {
-    document.getElementById("clear").innerHTML = "VIEW MORE";
+    document.getElementById("clear").innerHTML = "View More";
     const img = document.getElementById("image-gallery-1");
     img.src = "./assets/gallery-4-text-center.png";
+    var abc = '#collapseExample';
+    var toggle = "collapse";
+    var clear ="clearcontent('clear')";
+    document.getElementById('clear').innerHTML = '<a data-toggle="'+toggle+'" + onClick="'+clear+'" +  href="' + abc + '" > VIEW MORE </a>';
+    
 }
 document.getElementById("button").addEventListener('click',
 function (){
